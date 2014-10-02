fb077e40d0c9a34d265437236a9fbaa54f0e467d
mycrobe <jmulvane@cshl.edu>
2014-10-02 17:36:58 -0400

    Super-simple typeahead renders raw JSON from many search endpoints.

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * Created by mulvaney on 10/2/14.
+ */
+var express = require('express');
+var timer = require('response-time'); // adds X-Response-Time header
+var logger = require('express-logger');
+var Q = require('q');
+var http = require('q-io/http');
+var app = express();
+
+var searchEndpoints = [
+  {name: 'solr', url: 'http://gorgonzola.cshl.edu:8983/solr/grm-search/select?wt=json&indent=true&q='},
+  {name: 'gene', url: 'http://data.gramene.org/search/genes/select?q='},
+  {name: 'reactome', url: 'http://data.gramene.org/search/reactome/select?q='},
+  {name: 'cyc', url: 'http://data.gramene.org/search/cyc/select?q='}
+]
+
+app.use(express.static(__dirname + '/web'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use(logger({path: 'logs/server.log'}));
+app.use(timer());
+
+app.get('/search', function (rq, rs) {
+  var promisedSearchResponseBodies = searchEndpoints.map(function(searchEndpoint) {
+    // append the query parameter to the end of the url, then make call. We are returning a promise because
+    // http is from q-io
+    return http.read(searchEndpoint.url + rq.query.q);
+  });
+
+  // wait until all queries succeed and then merge them into a JSON response document
+  // and return to client
+  Q.all(promisedSearchResponseBodies).then(function (responseStrings) {
+    var result = {query: rq.query, hello: 'hello'};
+    for(var i = 0; i < responseStrings.length; i++) {
+      result[searchEndpoints[i].name] = JSON.parse('' + responseStrings[i]);
+    }
+    rs.send(result);
+  });
+});
+
+app.listen('3000');
